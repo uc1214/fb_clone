5cbc9ac8221523e5c26238cc703391daca6b00c9
Yushi Saijo <yushi.saijo@gmail.com>
2017-06-23 23:36:43 +0900

    revise locale for model and remove validate for topic_img

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,6 @@
 class Topic < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
-  validates :topic_img, presence: true
 
   mount_uploader :topic_img, ImgUploader
 
