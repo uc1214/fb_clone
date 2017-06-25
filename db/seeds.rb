2b179352729c991cb8da69690cb09d7a4dd70e2f
Yushi Saijo <yushi.saijo@gmail.com>
2017-06-25 23:09:25 +0900

    make seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Userテーブルのレコードの初期化
+User.delete_all
+
+#Blogテーブルのレコードの初期化
+Topic.delete_all
+
+#Comentテーブルのレコードの初期化
+Comment.delete_all
+
+100.times do |m|
+  email = Faker::Internet.email
+  name = "hoge"
+  User.create!(id: m,
+               email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+  Topic.create(
+    id: m,
+    title: "hoge",
+    content: "hogehogefugafuga",
+    user_id: m
+  )
+  Comment.create(
+    topic_id: m,
+    content: "fugahoge",
+    user_id: m
+  )
+end
