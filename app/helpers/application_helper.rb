6cc7380a30cee0b44acb36cc7861ff80a927e77f
Yushi Saijo <yushi.saijo@gmail.com>
2017-06-26 00:11:06 +0900

    revise image_url_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     unless user.provider.blank?
       img_url = user.image_url
     else
-      img_url = 'no_image.png'
+      img_url = '../assets/images/no_image.png'
     end
     image_tag(img_url, alt: user.name, :class => "img-profile")
   end
